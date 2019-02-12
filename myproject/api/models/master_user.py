7ef899a6d3dc50389e9f17605b4ea76cc8ced057
Yohandy Chiawanda <yohandych16@gmail.com>
2019-02-12 15:14:05 +0700

    Add employee id into master_user model

--- myproject/api/models/master_user.py
+++ myproject/api/models/master_user.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .master_employee import master_employee
 
 
 class master_user(models.Model):
@@ -8,6 +9,7 @@ class master_user(models.Model):
     user_level = models.IntegerField()
     user_type = models.IntegerField()
     last_login_date = models.DateField()
+    employee_id = models.OneToOneField(master_employee, on_delete=models.DO_NOTHING, blank=True, default=None)
 
     class Meta:
         db_table = 'MasterUser'
