a57ed07f20b4ff27694cd44048ae063632b6200f
Yohandy Chiawanda <yohandych16@gmail.com>
2019-02-28 15:14:34 +0700

    Signal to create rental stock card object completed and successfully tested Signal to create rental stock sn object upon object creation of rental stock card object is completed and successfully tested

--- myproject/api/views/another_views.py
+++ myproject/api/views/another_views.py
@@ -1,13 +1,12 @@
 from django.http import Http404
 
-from myproject.api.models import receiving_header, rental_stock_card, receiving_detail
+from myproject.api.models import receiving_header, rental_stock_card, receiving_detail, rental_stock_sn
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.dispatch import receiver, Signal
-from myproject.api.serializers import NestedReceivingHeaderSerializer, NestedStockCardSerializer, \
-    RentalStockCardSerializer
-import json
+from myproject.api.serializers import NestedReceivingHeaderSerializer, NestedStockCardSerializer
+import time
 
 # This view is purposely used for testing only
 
@@ -46,7 +45,10 @@ class NestedReceivingManagementDetails(APIView):
         serializer = NestedReceivingHeaderSerializer(header, data=request.data)
         if serializer.is_valid():
             serializer.save()
+            START = time.time()
             update_on_nested_serializer.send(sender=receiving_header, test=serializer.data)
+            end = time.time()
+            print(end - START)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -56,36 +58,46 @@ def yolo(sender, **kwargs):
     ReceivingHeaderData = kwargs['test']
     Detail_from_ReceivingHeaderData = ReceivingHeaderData['RDHeader']
 
-    # print(Detail_from_ReceivingHeaderData[1]['receiving_detail_id'])
 
-    for EachDetail in Detail_from_ReceivingHeaderData:
-        stockCardData = rental_stock_card.objects.create(item_master_id=1,
-                                                         location_id=ReceivingHeaderData['location_id'],
-                                                         qty=EachDetail['qty'],
-                                                         rental_header_id=None,
-                                                         rental_detail_id=None,
-                                                         receiving_header_id=receiving_header(
-                                                             EachDetail['receiving_header_id']),
-                                                         receiving_detail_id=receiving_detail(
-                                                             EachDetail['receiving_detail_id']))
-        if stockCardData:
-            # print("Success create object")
-            print(stockCardData)
-        else:
-            print("Failed create object")
 
-        # a = kwargs['test']
-    # b = a['RDHeader']
-    # c = json.dumps(b, indent=4)
-    # d = json.dumps(a, indent=4)
-    # e = json.loads(c)
-    # # print(e[0])
-    # for x in e:
-    #     temp = []
-    # if "1" is a['status']:
-    #     print('DRAFT')
-    # else:
-    #     print('APPROVED')
+
+    if "1" is ReceivingHeaderData['status']:
+        print('DRAFT, because the status is still DRAFT, so nothing happened XD')
+    else:
+        print('APPROVED')
+        for EachDetail in Detail_from_ReceivingHeaderData:
+            stockCardData = rental_stock_card.objects.create(item_master_id=1,
+                                                             location_id=ReceivingHeaderData['location_id'],
+                                                             qty=EachDetail['qty'],
+                                                             rental_header_id=None,
+                                                             rental_detail_id=None,
+                                                             receiving_header_id=receiving_header(
+                                                                 EachDetail['receiving_header_id']),
+                                                             receiving_detail_id=receiving_detail(
+                                                                 EachDetail['receiving_detail_id']))
+            if stockCardData:
+                print(stockCardData)
+                sn = []
+
+                # Move all SN in RDISN of each Detail into a variable called sn, then create rental stock sn objects for each rental stock card
+                for EachSNArray in EachDetail['RDISN']:
+                    temp = {}
+                    # print(EachSNArray)
+                    for keys, values in EachSNArray.items():
+                        # print("This is inside EachSNArray.items() = ", keys, values)
+                        temp[keys] = values
+                    sn.append(temp)
+
+                for SN in sn:
+                    stockSN = rental_stock_sn.objects.create(first_sn=SN['first_serial_number'],
+                                                             new_sn=SN['new_serial_number'],
+                                                             stock_card_id=stockCardData)
+                    if stockSN:
+                        print("Create Rental Stock SN object success!! the ID of this object is = ", stockSN)
+                    else:
+                        print("Failed to create Rental Stock SN Object")
+            else:
+                print("Failed create object")
 
 
 # View for nested serializer on stock mannagement
