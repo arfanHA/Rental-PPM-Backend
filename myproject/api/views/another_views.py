4609764aa277dd2162ade56bb97c45377077aa50
Yohandy Chiawanda <yohandych16@gmail.com>
2019-03-01 16:16:33 +0700

    Minor fix

--- myproject/api/views/another_views.py
+++ myproject/api/views/another_views.py
@@ -49,7 +49,7 @@ class NestedReceivingManagementDetails(APIView):
         serializer = NestedReceivingHeaderSerializer(header, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            # update_on_nested_serializer.send(sender=receiving_header, test=serializer.data)
+            update_on_nested_serializer.send(sender=receiving_header, test=serializer.data)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
