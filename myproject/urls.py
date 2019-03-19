09f095aeb5d78f65bb3ac84ed2060244b040d2a1
Yohandy Chiawanda <yohandych16@gmail.com>
2019-03-20 05:35:30 +0700

    Add new API Documentation (Listing all available api to use)

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = 'Rental API'
+API_DESCRIPTION = '...'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myproject.api.urls'))
+    path('api/', include('myproject.api.urls')),
+    url(r'docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
 
