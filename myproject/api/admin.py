56dcfb725317c0b77208866e7a3fbe1cb8de15d6
Yohandy Chiawanda <yohandych16@gmail.com>
2019-04-22 16:07:08 +0700

    Add invoice header and detail into admin site

--- myproject/api/admin.py
+++ myproject/api/admin.py
@@ -15,8 +15,10 @@ admin.site.register(master_employee)
 admin.site.register(receiving_detail_sn)
 admin.site.register(rental_order_header)
 admin.site.register(rental_order_detail)
-admin.site.register(rental_header) # Rental Management starts here
+admin.site.register(rental_header)  # Rental Management starts here
 admin.site.register(rental_detail)
-admin.site.register(rental_stock_card) # Stock Management starts here
+admin.site.register(rental_stock_card)  # Stock Management starts here
 admin.site.register(rental_stock_sn)
 admin.site.register(stock_sn_history)
+admin.site.register(invoice_header)
+admin.site.register(invoice_detail)
