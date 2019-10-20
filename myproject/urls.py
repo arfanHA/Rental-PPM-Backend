b305a70abf955da2844886fe4303c8549a453b2f
Gusan <gusanhite@gmail.com>
2019-10-20 10:31:16 +0700

    Menambahkan URL API user info

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.documentation import include_docs_urls
 from rest_framework.authtoken.views import obtain_auth_token
+from myproject.api.views.another_views import CustomAuthToken
 
 API_TITLE = 'Rental API'
 API_DESCRIPTION = '...'
@@ -10,7 +11,7 @@ API_DESCRIPTION = '...'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myproject.api.urls')),
-    path('api-token-auth/', obtain_auth_token, name="api_token_auth"),
+    path('api-token-auth/', CustomAuthToken.as_view(), name="api_token_auth"),
     url(r'docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
 
