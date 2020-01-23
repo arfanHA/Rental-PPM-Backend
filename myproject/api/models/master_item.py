a3a57d34663263fd7b6dabe971db751215117412
gusanhite <gusanhite@gmail.com>
2020-01-23 14:45:16 +0700

    add serial_numb in master item

--- myproject/api/models/master_item.py
+++ myproject/api/models/master_item.py
@@ -11,12 +11,13 @@ class master_item(models.Model):
     barcode = models.CharField(max_length=100, blank=True)
     name = models.CharField(max_length=200, unique=True)
     alias_name = models.CharField(max_length=200, blank=True)
-    master_group_id = models.ForeignKey(master_group_item, on_delete=models.DO_NOTHING)
+    master_group_id = models.ForeignKey(master_group_item, on_delete=models.DO_NOTHING,related_name='MasterGroup')
     uom_id = models.ForeignKey(master_uom, on_delete=models.DO_NOTHING)
     merk_id = models.ForeignKey(master_merk, on_delete=models.DO_NOTHING)
     price1 = models.CharField(max_length=200)
     price2 = models.CharField(max_length=200, blank=True)
     price3 = models.CharField(max_length=200, blank=True)
+    serial_number = models.BooleanField(default=True)
 
     class Meta:
         db_table = 'MasterItem'
