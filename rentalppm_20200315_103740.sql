a95f36843b6dbca4a5cd943d4a787a08e7521f43
olede <denihhandoko@gmail.com>
2020-03-15 10:43:23 +0700

    dump lagi

--- rentalppm_20200315_103740.sql
+++ rentalppm_20200315_103740.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: rentalppm; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE rentalppm WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
+
+
+ALTER DATABASE rentalppm OWNER TO postgres;
+
+\connect rentalppm
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 --
